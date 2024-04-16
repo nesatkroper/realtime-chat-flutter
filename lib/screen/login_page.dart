1b2a537d7a6b3c1474389c444bc1e5b30d689b50
Baby Bob <phanunsuon@gmail.com>
2024-04-16 21:50:10 +0700

    1.03

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,4 +1,85 @@
+// import 'package:flutter/material.dart';
+// import 'package:realtime_chat/widget/custom_form.dart';
+
+// class LoginPage extends StatefulWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   State<LoginPage> createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _buildUI(),
+//     );
+//   }
+
+//   Widget _buildUI() {
+//     return SafeArea(
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(
+//           horizontal: 15,
+//           vertical: 20,
+//         ),
+//         child: Column(
+//           children: [
+//             _headerText(),
+//             _loginForm(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _headerText() {
+//     return SizedBox(
+//       width: MediaQuery.sizeOf(context).width,
+//       child: const Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         mainAxisSize: MainAxisSize.max,
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Text(
+//             'Hi, Welcome Back.',
+//             style: TextStyle(
+//               fontSize: 20,
+//               fontWeight: FontWeight.w800,
+//             ),
+//           ),
+//           Text(
+//             "Hello again, you've been missed",
+//             style: TextStyle(
+//               fontSize: 15,
+//               fontWeight: FontWeight.w500,
+//               color: Colors.grey,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _loginForm() {
+//     return Container(
+//       height: MediaQuery.sizeOf(context).height * .4,
+//       margin: EdgeInsets.symmetric(
+//         vertical: MediaQuery.sizeOf(context).height * .05,
+//       ),
+//       child: const Form(
+//         child: Column(
+//           children: [
+//             CustomFormField(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:realtime_chat/widget/custom_form.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,6 +93,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: _buildUI(),
     );
   }
@@ -27,6 +109,7 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             _headerText(),
             _loginForm(),
+            _createAnAccountLink(),
           ],
         ),
       ),
@@ -67,12 +150,64 @@ class _LoginPageState extends State<LoginPage> {
       margin: EdgeInsets.symmetric(
         vertical: MediaQuery.sizeOf(context).height * .05,
       ),
-      child: const Form(
+      child: Form(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CustomFormField(),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * .1,
+              hintText: 'Email',
+            ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * .1,
+              hintText: 'Password',
+            ),
+            _loginButton(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: MaterialButton(
+        onPressed: () {},
+        color: Theme.of(context).colorScheme.primary,
+        child: const Text(
+          'Login',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _createAnAccountLink() {
+    return const Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "Don't have an account?",
+          ),
+          Text(
+            "Sign Up",
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widget/custom_form.dart
+++ lib/widget/custom_form.dart
@@ -1,10 +1,33 @@
+// import 'package:flutter/material.dart';
+
+// class CustomFormField extends StatelessWidget {
+//   const CustomFormField({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextFormField();
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class CustomFormField extends StatelessWidget {
-  const CustomFormField({super.key});
+  final String hintText;
+  final double height;
+
+  const CustomFormField(
+      {super.key, required this.hintText, required this.height});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return SizedBox(
+      height: height,
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: const OutlineInputBorder(),
+        ),
+      ),
+    );
   }
 }
