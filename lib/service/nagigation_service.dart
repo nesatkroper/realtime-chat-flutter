6c733d8980f2c9817e96cfda7b7195a0c9a0f50a
Baby Bob <phanunsuon@gmail.com>
2024-04-20 01:03:29 +0700

    1.05

---
+++ lib/service/nagigation_service.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:realtime_chat/screen/login_page.dart';
+
+class NavigationService {
+  late GlobalKey<NavigatorState> _navigatorKey;
+
+  final Map<String, Widget Function(BuildContext)> _routes = {
+    "/login": (context) => LoginPage(),
+  };
+
+  GlobalKey<NavigatorState>? get navigatorKey {
+    return _navigatorKey;
+  }
+
+  Map<String, Widget Function(BuildContext)> get routes {
+    return _routes;
+  }
+
+  NavigationService() {
+    _navigatorKey = GlobalKey<NavigatorState>();
+  }
+
+  void pushNamed(String routeName) {
+    _navigatorKey.currentState?.pushNamed(routeName);
+  }
+
+  void pushReplacementNamed(String routeName) {
+    _navigatorKey.currentState?.pushReplacementNamed(routeName);
+  }
+}
